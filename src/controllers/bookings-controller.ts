519648699bdef5a75e671283401f2e91c46043f9
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-04-29 12:33:05 -0300

    feat: POST /booking/:bookingId

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -34,3 +34,22 @@ export async function postBooking(req: AuthenticatedRequest, res: Response) {
     return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
+
+export async function updateBooking(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { roomId } = req.body as Record<string, number>;
+  const bookingId = Number(req.params.bookingId) as number;
+
+  try {
+    const result = await bookingsService.update(userId, roomId, Number(bookingId));
+    return res.send(result);
+  } catch (error) {
+    if (error.name === 'NotFoundError') {
+      return res.sendStatus(httpStatus.NOT_FOUND);
+    }
+    if (error.name === 'ForbiddenError') {
+      return res.status(httpStatus.FORBIDDEN).send(error.message);
+    }
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
--- src/services/bookings-service/index.ts
+++ src/services/bookings-service/index.ts
@@ -5,14 +5,7 @@ import roomRepository from '@/repositories/room-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 import ticketRepository from '@/repositories/ticket-repository';
 
-async function getBooking(userId: number): Promise<BookIdWithRoom> {
-  const booking = await bookingRepository.findBookByUserId(userId);
-  if (!booking) throw notFoundError();
-
-  return booking;
-}
-
-async function create(userId: number, roomId: number): Promise<{ bookingId: number }> {
+async function verifyPermissionBooking(userId: number, roomId: number): Promise<void> {
   const enrollment = await enrollmentRepository.findByUserId(userId);
   if (!enrollment) throw forbiddenError('User does not have an enrollment');
 
@@ -29,8 +22,34 @@ async function create(userId: number, roomId: number): Promise<{ bookingId: numb
 
   const countBookingsByRoomId = await bookingRepository.countBookingsByRoomId(roomId);
   if (countBookingsByRoomId === room.capacity) throw forbiddenError('This room is not available');
+}
+
+async function verifyUserBooking(userId: number, bookingId: number): Promise<void> {
+  const booking = await bookingRepository.findBookByUserId(userId);
+  if (!booking || booking.id !== bookingId) throw forbiddenError('No Permission for this operation.');
+}
+
+async function getBooking(userId: number): Promise<BookIdWithRoom> {
+  const booking = await bookingRepository.findBookByUserId(userId);
+  if (!booking) throw notFoundError();
+
+  return booking;
+}
+
+async function create(userId: number, roomId: number): Promise<{ bookingId: number }> {
+  await verifyPermissionBooking(userId, roomId);
 
   const booking = await bookingRepository.create({ userId, roomId });
+
+  return { bookingId: booking.id };
+}
+
+async function update(userId: number, roomId: number, bookingId: number): Promise<{ bookingId: number }> {
+  await verifyPermissionBooking(userId, roomId);
+
+  await verifyUserBooking(userId, bookingId);
+
+  const booking = await bookingRepository.update(bookingId, roomId);
 
   return { bookingId: booking.id };
 }
@@ -40,6 +59,7 @@ export type BookIdWithRoom = Pick<Booking, 'id'> & { Room: Room };
 const bookingsService = {
   getBooking,
   create,
+  update,
 };
 
 export default bookingsService;
